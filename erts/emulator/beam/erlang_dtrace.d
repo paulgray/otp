626402cc373cdb8138d0a303cafc38a6b76ae9f1
Dustin Sallings <dustin@spy.net>
2011-09-10 15:37:54 -0700

    Added call return.

--- erts/emulator/beam/erlang_dtrace.d
+++ erts/emulator/beam/erlang_dtrace.d
@@ -4,6 +4,8 @@ provider erlang {
 
     /* PID, Module, Function, Arity */
     probe function__entry(char *, char*, char*, int);
+    probe function__return(char *, char*, char*, int);
+
     probe bif__entry(char *, char*, char*, int);
     probe nif__entry(char *, char*, char*, int);
 
