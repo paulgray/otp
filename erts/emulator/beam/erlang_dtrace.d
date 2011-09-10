935fb333c691d053e8dc11da33777f0fb7e3b644
Dustin Sallings <dustin@spy.net>
2011-09-10 15:50:24 -0700

    Working send (with process names).

--- erts/emulator/beam/erlang_dtrace.d
+++ erts/emulator/beam/erlang_dtrace.d
@@ -1,5 +1,5 @@
 provider erlang {
-    probe send(void *sender, void*receiver, uint32_t size);
+    probe send(char *sender, char *receiver, uint32_t size);
     probe copy_struct(uint32_t size);
 
     probe copy_object(char *proc, uint32_t size);
